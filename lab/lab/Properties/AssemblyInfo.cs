cc8f29170fac2e61fff760b9e906e6759d532790
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-03-18 13:58:52 -0300

    lala

--- lab/lab/Properties/AssemblyInfo.cs
+++ lab/lab/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("lab1")]
+[assembly: AssemblyTitle("lab")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
