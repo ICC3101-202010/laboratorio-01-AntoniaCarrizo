dd3694f04552e5e4ba96aa542275ec252715463b
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-03-19 11:03:09 -0300

    Prueba-gitignore

---
+++ Laboratorio-Antonia/Laboratorio-Antonia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratorioAntonia
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
